0c33c0955a99bfb3c6cc5f09b5d2af3702dca0d3
Neira P <neyra140719@gmail.com>
2025-05-22 14:45:42 -0600

    Se añaden comentarios

---
+++ assets/js/app.js
@@ -0,0 +1,25 @@
+const baseEndpoint = 'https://api.github.com';
+const usersEndpoint = `${baseEndpoint}/users`;
+const $n = document.querySelector('.name');//agregue el punto para que tuviera bien la etiqueta 
+const $b = document.querySelector('#blog');
+const $l = document.querySelector('.location');
+const data = await response.json();// lo agregué para que el data funcione 
+
+//agregé async para que await funcionara
+function async displayUser(username) {
+  $n.textContent = 'cargando...';
+  const response = await fetch(`${usersEndpoint}/${username}`);
+  console.log(data);
+  $n.textContent = `${data.name}`;//le puse backticks a los tres para hacerlos template strings
+  $b.textContent = `${data.blog}`;
+  $l.textContent = `${data.location}`;
+}
+
+function handleError(err) {
+  console.log('OH NO!');
+  console.log(err);
+  //le puse un signo de $ antes de la n para que lo llamara correctamente 
+  $n.textContent = `Algo salió mal: ${err}`
+}
+
+displayUser('stolinski').catch(handleError);
